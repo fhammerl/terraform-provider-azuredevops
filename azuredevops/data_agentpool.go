3cd07c4424f66580d7e34fe0f460553d3de43c7f
Ferenc Hammerl <ferenchammerl@outlook.com>
2020-05-21 22:53:53 +0000

    Add methods to read agent pool
    Co-authored-by: Eliise Seling <Eliise.Seling@microsoft.com>

--- azuredevops/data_agentpool.go
+++ azuredevops/data_agentpool.go
@@ -1,37 +1,59 @@
 package azuredevops
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"fmt"
 
-	"strconv"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/taskagent"
+	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/utils/config"
 )
 
 func dataAzureAgentPool() *schema.Resource {
+	baseSchema := resourceAzureAgentPool()
+	for k, v := range baseSchema.Schema {
+		if k != "name" {
+			baseSchema.Schema[k] = &schema.Schema{
+				Type:     v.Type,
+				Computed: true,
+			}
+		}
+	}
+
 	return &schema.Resource{
-		Read: resourceAzureAgentPoolReadByPoolId,
-		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:     schema.TypeString,
-				Computed: true},
-			"pool_type": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"auto_provision": {
-				Type:     schema.TypeBool,
-				Computed: true,
-			},
-			"pool_id": {
-				Type:     schema.TypeInt,
-				Required: true,
-			},
-		},
+		Read:   dataSourceAgentPoolRead,
+		Schema: baseSchema.Schema,
 	}
 }
 
-func resourceAzureAgentPoolReadByPoolId(d *schema.ResourceData, m interface{}) error {
-	poolId := strconv.Itoa(d.Get("pool_id").(int))
-	d.SetId(poolId)
-	err := resourceAzureAgentPoolRead(d, m)
+func dataSourceAgentPoolRead(d *schema.ResourceData, m interface{}) error {
+	agentPoolName := d.Get("name").(string)
+	clients := m.(*config.AggregatedClient)
+
+	agentPool, err := getAgentPoolByName(clients, &agentPoolName)
+	if err != nil {
+		return fmt.Errorf("Error getting agent pool by name: %v", err)
+	}
+
+	flattenAzureAgentPool(d, agentPool)
 	return err
+}
+
+func getAgentPoolByName(clients *config.AggregatedClient, name *string) (*taskagent.TaskAgentPool, error) {
+	agentPools, err := clients.TaskAgentClient.GetAgentPools(clients.Ctx, taskagent.GetAgentPoolsArgs{
+		PoolName: name,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(*agentPools) > 1 {
+		return nil, fmt.Errorf("Found multiple agent pools for name: %s. Agent pools found: %v", *name, agentPools)
+	}
+
+	if len(*agentPools) == 0 {
+		return nil, fmt.Errorf("Unable to find agent pool with name: %s", *name)
+	}
+
+	return &(*agentPools)[0], nil
 }
